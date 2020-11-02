3697e3a4792c8f72761988f236480c4cf52d942d
Anatoly <antauren@yandex.ru>
2020-11-02 03:33:54 +0300

    Add paginator.

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 import os
+import math
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -18,12 +19,15 @@ def rebuild():
     )
     template = env.get_template('template.html')
     data = load_json('downloads/data.json')
+    for book in data:
+        book['img_src'] = os.path.join('/', book['img_src'])
 
     pages_dir = 'pages'
     os.makedirs(pages_dir, exist_ok=True)
 
     split_length = 10
     chunked_data = list(more_itertools.chunked(data, split_length))
+    pages_count = math.ceil(len(data) / split_length)
 
     for num, page_data in enumerate(chunked_data, start=1):
         page_data_list = list(page_data)
@@ -32,6 +36,18 @@ def rebuild():
         rendered_page = template.render(
             book_groups=more_itertools.chunked(page_data_list,
                                                data_length // 2 + 1 if data_length % 2 else data_length // 2),
+            pages=[
+                {
+                    'num': page_num,
+                    'url': os.path.join('/', pages_dir, 'index{}.html'.format(page_num))
+                }
+                for page_num in range(1, pages_count + 1)
+            ],
+            current_page=num,
+            next=num != pages_count,
+            previous=num != 1,
+            next_url=os.path.join('/', pages_dir, 'index{}.html'.format(num + 1)),
+            previous_url=os.path.join('/', pages_dir, 'index{}.html'.format(num - 1))
         )
 
         page_path = os.path.join(pages_dir, 'index{}.html'.format(num))
@@ -42,6 +58,7 @@ def rebuild():
 
 rebuild()
 server = Server()
+server.serve(default_filename='pages/index1.html')
 
 server.watch('template.html', rebuild)
 
